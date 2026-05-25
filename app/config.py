8220222edb6d1d3a38bb9fc0ec68414be97fa254
Hardick Chatterjee <hardickchatterjee@Hardicks-MacBook-Pro.local>
2026-05-25 13:32:23 -0700

    Added more youtube channels

--- app/config.py
+++ app/config.py
@@ -1,4 +1,8 @@
 YOUTUBE_CHANNELS = [
     # "UCn8ujwUInbJkBhffxqAPBVQ", # Dave Ebbelaar
     "UCawZsQWqfGSbCI5yjkdVkTA", # Matthew Berman
+    "UCVMe_3tsQam1PuNNbHRgIHQ", # AI Explained
+    "UCbmNph6atAoGfqLoCL_duAg", # Andrej Karpathy
+    "UCWX3yGbODI3HMKMCxYHMKHg", # Yannic Kilcher
+    "UCnUYZLuoy1rq1aVMwx4aTzw", # Two Minute Papers
 ]
