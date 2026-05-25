0b17c115db0f1a1407449b1f6cb94964181fd2e4
Hardick Chatterjee <hardickchatterjee@Hardicks-MacBook-Pro.local>
2026-05-25 14:08:40 -0700

    Revert "Added more youtube channels"
    This reverts commit 8220222edb6d1d3a38bb9fc0ec68414be97fa254.

--- app/config.py
+++ app/config.py
@@ -1,8 +1,4 @@
 YOUTUBE_CHANNELS = [
     # "UCn8ujwUInbJkBhffxqAPBVQ", # Dave Ebbelaar
     "UCawZsQWqfGSbCI5yjkdVkTA", # Matthew Berman
-    "UCVMe_3tsQam1PuNNbHRgIHQ", # AI Explained
-    "UCbmNph6atAoGfqLoCL_duAg", # Andrej Karpathy
-    "UCWX3yGbODI3HMKMCxYHMKHg", # Yannic Kilcher
-    "UCnUYZLuoy1rq1aVMwx4aTzw", # Two Minute Papers
 ]
